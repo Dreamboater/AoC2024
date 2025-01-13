804892fa11d6bf31eafe780ee6b0a10264da4c81
Dreamboater <adrian.t.vandervaart@gmail.com>
2025-01-12 23:15:37 -0500

    Midter Practice

---
+++ Question 1.py
@@ -0,0 +1,7 @@
+# https://cemc.uwaterloo.ca/sites/default/files/documents/2024/2024CCCJrProblems.html
+
+#We will be doing question #2 All the files labelled "j2.0*.in" will be you different sample
+# inputs you can try. All files labelled "j2.0*.out" are the solutions to check your work.
+# The pdf is a description of the problem
+
+
---
+++ Question 2.py
@@ -0,0 +1,7 @@
+# This problem is taken from: https://adventofcode.com/2024/day/1
+# Your puzzle answer was 1722302.
+#
+file = open("Question 2", "r") #opens a file
+line = file.readline() #reads a line of text from the file
+
+
